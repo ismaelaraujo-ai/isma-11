5a69abdaf70b43305d081cd04d2b2b17ffc1993b
ismael <ismaelaraujov.2209@gmail.com>
2026-04-23 22:08:45 -0300

    conclusao do exercicio IMC

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+double altura, peso, IMC; 
+
+Console.WriteLine("Altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Peso (kg)..: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+IMC = peso / Math.Pow(altura, 2);
+
+Console.WriteLine($"\nIMC: {IMC:N1} kg/m²");
